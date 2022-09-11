b569ad63074207a7dae82c4188d2c68bb4d5b676
yoelhaim <youssefelhaimer8@gmail.com>
2022-09-11 21:46:52 +0100

    add n ofcontact :rocket:

--- ex01/Phonebook.hpp
+++ ex01/Phonebook.hpp
@@ -1,7 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Phonebook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yoelhaim <yoelhaim@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/11 13:26:45 by yoelhaim          #+#    #+#             */
+/*   Updated: 2022/09/11 20:29:27 by yoelhaim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #pragma once
 
 #include <iostream>
-#include <cstring>
 #include "Contact.hpp"
 #define PRINT std::cout 
 
@@ -9,8 +20,12 @@ class PhoneBook
 {
 	private :
 		Contact contacts[8];
+		int		numberOfContact;
 	public :
+		PhoneBook();
 		void addContact(STR firstName, STR lastName, STR nickName, STR phoneNumber, STR darksetSecret);
-		void getContact(int id);
+		void getContact();
+		void searchContact(int id, int type);
+		
 
 };
