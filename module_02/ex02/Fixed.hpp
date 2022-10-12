57dd510d19d69ad5ebc0790f41bbd7a0ea076ec7
yoelhaim <youssefelhaimer8@gmail.com>
2022-10-12 09:49:05 +0100

    header

--- module_02/ex02/Fixed.hpp
+++ module_02/ex02/Fixed.hpp
@@ -3,24 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   Fixed.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yoelhaim <yoelhaim@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pro <pro@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/11 20:40:43 by yoelhaim          #+#    #+#             */
-/*   Updated: 2022/10/11 20:45:17 by yoelhaim         ###   ########.fr       */
+/*   Created: 2022/09/29 19:17:21 by yoelhaim          #+#    #+#             */
+/*   Updated: 2022/10/09 21:53:14 by pro              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#pragma once
 #include <iostream>
-#include <cmath>
 
 class Fixed
 {
 private:
-	
+	int number;
+	static const int bits = 8;
+
 public:
 	Fixed();
-	
+	Fixed(const int number);
+	Fixed(const float number);
+	Fixed(const Fixed &t);
+	Fixed &operator=(const Fixed &t);
 	~Fixed();
+	int toInt(void) const;
+	float toFloat(void) const;
+	int getRawBits() const;
 };
 
+std::ostream &operator<<(std::ostream &outpot, const Fixed &t);
